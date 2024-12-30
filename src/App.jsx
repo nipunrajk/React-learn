ed3ec96826888e7581d2aaa53cabaa835e11b22d
Nipunraj <nipunraj@mozilor.com>
2024-12-31 03:25:06 +0530

    UseReducer hook example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import UserProfile from "./component/UserProfile";
-import { UserProvider } from "./UserContext";
-import UpdateUser from "./component/UpdateUser";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+
+    case "reset":
+      return { ...state, count: 0 };
+
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <UserProvider>
-      <UserProfile />
-      <UpdateUser />
-    </UserProvider>
+    <div>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset " })}>Reset</button>
+
+      <h1>Count : {state.count}</h1>
+    </div>
   );
 }
 
