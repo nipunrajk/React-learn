10df473eb775e3bb226329638690ed17ae69ff8b
Nipunraj <nipunraj@mozilor.com>
2024-12-31 04:25:08 +0530

    challange

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
 import React from "react";
-import { useReducer } from "react";
-
-const initialState = { count: 0 };
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, count: state.count + 1 };
-
-    case "decrement":
-      return { ...state, count: state.count - 1 };
-
-    case "reset":
-      return { ...state, count: 0 };
-
-    default:
-      return state;
-  }
-};
+import Counter from "./component/Counter";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "reset " })}>Reset</button>
-
-      <h1>Count : {state.count}</h1>
+      <Counter />  
     </div>
   );
 }
