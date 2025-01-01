09a5f2095d8b01a2b0837087f395f1a61e7522d9
Nipunraj <nipunraj@mozilor.com>
2025-01-01 16:05:56 +0530

    CustomHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import useFetch from "./useFetch";
 
 function App() {
-  return <div></div>;
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+  return (
+    <div>{data && data.map((item) => <li key={item.id}>{item.title}</li>)}</div>
+  );
 }
 
 export default App;
